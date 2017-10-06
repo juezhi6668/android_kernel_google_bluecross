3320a27febb105269c71ee798c301f592bb534f8
Martin KaFai Lau <kafai@fb.com>
2017-10-05 21:52:12 -0700

    BACKPORT: bpf: Use char in prog and map name
    Instead of u8, use char for prog and map name.  It can avoid the
    userspace tool getting compiler's signess warning.  The
    bpf_prog_aux, bpf_map, bpf_attr, bpf_prog_info and
    bpf_map_info are changed.
    
    Change-Id: I599a8f1eccb0d63aa8d680b771fff1580c69cf75
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Cc: Jakub Kicinski <jakub.kicinski@netronome.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -68,7 +68,7 @@ struct bpf_map {
 #ifdef CONFIG_SECURITY
 	void *security;
 #endif
-	u8 name[BPF_OBJ_NAME_LEN];
+	char name[BPF_OBJ_NAME_LEN];
 };
 
 /* function argument constraints */
@@ -203,7 +203,7 @@ struct bpf_prog_aux {
 	void *security;
 #endif
 	u64 load_time; /* ns since boottime */
-	u8 name[BPF_OBJ_NAME_LEN];
+	char name[BPF_OBJ_NAME_LEN];
 	union {
 		struct work_struct work;
 		struct rcu_head	rcu;
